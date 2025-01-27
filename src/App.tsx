c96a5ad74fc89d575b66239d5faaa0539e5b62db
Erwin Daniel Arancibia <earancibia@falabella.cl>
2025-01-26 22:47:09 -0300

    add userRegister component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import UserRegister from "./components/UserRegister"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>Scheduler</h1>
+      <UserRegister />
+    </>
+  )
+}
+
+export default App
